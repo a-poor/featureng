05039052f92cf4b3b8971966be6f2a29ec927488
a-poor <45295232+a-poor@users.noreply.github.com>
2020-12-11 16:34:54 -0500

    Updating requirements and gh workflow

--- featureng/__init__.py
+++ featureng/__init__.py
@@ -3,3 +3,6 @@ from . import datetime
 from . import transform
 from . import scale
 from . import interaction
+
+from . import external
+from . import bq
--- featureng/external.py
+++ featureng/external.py
@@ -4,6 +4,8 @@ import json
 
 import requests
 
+from .exceptions import ApiRequestError, ApiResponseError
+
 
 class OpenWeather:
     def __init__(self,key=None):
@@ -58,23 +60,3 @@ class UsCensus:
             raise AuthError("US Census API")
         self.key = key
         self.base_url = "https://api.census.gov/data/"
-
-
-
-##### Exceptions #####
-
-class AuthError(Exception):
-    def __init__(self,service):
-        super().__init__(service)
-        self.service = service
-
-    def __str__(self):
-        up = "_".join(self.service.upper().split(" ")) + "_KEY"
-        msg = f"{self.service} key is required. Pass as argument or set environment variable '{up}'."
-        return msg
-
-class ApiRequestError(Exception):
-    pass
-
-class ApiResponseError(Exception):
-    pass
