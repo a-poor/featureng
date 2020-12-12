49f56f08d6e0ea7c42cb1ec9c8d8c3d4e97ac27a
a-poor <45295232+a-poor@users.noreply.github.com>
2020-12-12 12:27:32 -0500

    Adding more credential authentication tests

--- test_bq.py
+++ test_bq.py
@@ -3,6 +3,17 @@
 
 def test_gcp_auth():
     import os
+    import json
+    from pathlib import Path
     from google.cloud import bigquery
-    creds = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+    # Get the path to the credentials
+    creds = Path(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+    assert creds.exists()
+
+    # Try to load the json file
+    with creds.open() as f:
+        json.load(f)
+
+    # Try to authenticate
     client = bigquery.Client.from_service_account_json(creds)
