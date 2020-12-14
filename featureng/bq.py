ba72b5a2667caa754216f8e3382e307ab90f56ca
a-poor <45295232+a-poor@users.noreply.github.com>
2020-12-13 20:40:44 -0500

    Updating gh workflow, BQ tests, and adding BQ Census client class

--- featureng/bq.py
+++ featureng/bq.py
@@ -1,14 +1,38 @@
 
+import pandas as pd
 from google.cloud import bigquery
 
+from . import exceptions
 
-class BigQueryLoader:
-    def __init__(self,project=None,credentials=None):
-        self.auth = auth
-        self.client = bigquery.Client(
-            project=project,
-            credentials=credentials)
+from typing import Union, List, Dict, Sequence
 
-    def _query(self,q,*args,**kwargs):
+
+class BigQueryClient:
+    def __init__(self, client: Union[str, bigquery.client.Client] = None):
+        """BigQuery client
+
+        :param client:
+        """
+        if isinstance(client,bigquery.client.Client):
+            self.client = client
+        elif isinstance(client,str):
+            self.client = bigquery.Client.from_service_account_json(client)
+        else:
+            self.client = bigquery.Client()
+
+    def _query(self, q, *args, **kwargs):
         res = self.client.query(q,*args,**kwargs)
         return [dict(r) for r in res]
+
+
+class CensusAcsClient(BigQueryClient):
+    def __init__(self):
+        super().__init__(*args,**kwargs)
+        self.table_ids = self.getTableNames()
+
+    def getTableNames(self):
+        return [t.table_id for t in self.client.list_tables(
+            "bigquery-public-data.census_bureau_acs")]
+
+    def query(self):
+        pass
--- test_bq.py
+++ test_bq.py
@@ -1,19 +1,15 @@
 
+import json
+from pathlib import Path
+from google.cloud import bigquery
 # import featureng
 
 def test_gcp_auth():
-    import os
-    import json
-    from pathlib import Path
-    from google.cloud import bigquery
-
     # Get the path to the credentials
     creds = Path("./keyfile.json")
     assert creds.exists()
-
     # Try to load the json file
     with creds.open() as f:
         json.load(f)
-
     # Try to authenticate
     client = bigquery.Client.from_service_account_json(creds)
